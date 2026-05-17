e1db587710e7c739d40ca2411c39dedaf33e85cb
kultapon <kultsecond@gmail.com>
2026-05-17 18:16:24 +0300

    added auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import {
   RouterProvider,
 } from "react-router-dom";
-
 import { router } from "./app/router/router";
-
 import { QueryProvider } from "./app/providers/query-provider";
-
+import { AuthProvider } from "./app/providers/auth-provider";
 import { setupInterceptors } from "./shared/api/setup-interceptors";
 
 setupInterceptors();
@@ -16,7 +13,13 @@ setupInterceptors();
 ReactDOM.createRoot(
   document.getElementById("root")!,
 ).render(
-  <QueryProvider>
-    <RouterProvider router={router} />
-  </QueryProvider>,
+  <React.StrictMode>
+    <QueryProvider>
+      <AuthProvider>
+        <RouterProvider
+          router={router}
+        />
+      </AuthProvider>
+    </QueryProvider>
+  </React.StrictMode>,
 );
